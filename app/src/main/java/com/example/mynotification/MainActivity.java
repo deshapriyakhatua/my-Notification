308a8ae376af1b0019e245f54c58685abf6229ca
deshapriya <deshapriyakhatua@gmail.com>
2022-12-10 11:56:44 +0530

    notification channel created

--- app/src/main/java/com/example/mynotification/MainActivity.java
+++ app/src/main/java/com/example/mynotification/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.mynotification;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private static int NOTIFICATION_ID = 1;
+    private static String NOTIFICATION_CHANNEL_ID = "1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       // Drawable drawable = ResourcesCompat.getDrawable(getResources(),R.drawable.ic_baseline_circle_notifications_24,null);
+        //BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_baseline_circle_notifications_24);;
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification notification = new Notification.Builder(MainActivity.this)
+                .setLargeIcon(largeIcon)
+                .setSmallIcon(R.drawable.ic_baseline_circle_notifications_24)
+                .setContentText("new content text")
+                .setSubText("new subtext")
+                .setChannelId(NOTIFICATION_CHANNEL_ID)
+                .build();
+
+        notificationManager.createNotificationChannel(new NotificationChannel(NOTIFICATION_CHANNEL_ID,"General",NotificationManager.IMPORTANCE_HIGH));
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                notificationManager.notify(NOTIFICATION_ID,notification);
+            }
+        });
     }
 }
